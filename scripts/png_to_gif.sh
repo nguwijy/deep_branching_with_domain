7f316a41c8d69d90981a00ba4fc3f5b7d6efad8b
nguwijy <nguwijy@hotmail.com>
2022-06-07 11:33:29 +0800

    test: quiver plot on top of color map

--- scripts/png_to_gif.sh
+++ scripts/png_to_gif.sh
@@ -1,7 +1,5 @@
 # convert png files in the directory to gif
 # bash png_to_gif /path/to/plot/directory
 cd $1
-files=$(ls -t *velocity_color.png)
-convert -delay 20 -loop 0 -dispose Background $files velocity_color.gif
-files=$(ls -t *velocity_quiver.png)
-convert -delay 20 -loop 0 -dispose Background $files velocity_quiver.gif
+files=$(ls -t *velocity.png)
+convert -delay 20 -loop 0 -dispose Background $files velocity.gif
