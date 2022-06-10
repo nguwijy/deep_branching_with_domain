fa2e56b7a964186dcfbdcb055b8a03b0c669ce92
nguwijy <nguwijy@hotmail.com>
2022-06-10 10:07:36 +0800

    test: update rotating flow and poiseuille flow, and clean the log directory

--- scripts/png_to_gif.sh
+++ scripts/png_to_gif.sh
@@ -1,5 +1,5 @@
 # convert png files in the directory to gif
 # bash png_to_gif /path/to/plot/directory
 cd $1
-files=$(ls -t *velocity.png)
+files=$(ls -t t[0-9]*velocity.png)
 convert -delay 20 -loop 0 -dispose Background $files velocity.gif
